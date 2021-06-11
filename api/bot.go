f050983492f385dcdad91eb63b32ee73787a0e01
sonoflope <jonathanlopez@hotmail.ca>
2021-06-11 01:07:49 -0400

    Update api/bot.go to fix invalid memory address error.

--- api/bot.go
+++ api/bot.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -27,9 +29,9 @@ type Payload struct {
 type DiscordPayload struct{}
 
 type MattermostPayload struct {
-	Text      string `json:"text"`
-	Username  string `json:"user_name"`
-	UserID    string `json:"user_id"`
-	Token     string `json:"token"`
-	Timestamp string `json:"timestamp"`
+	Text      string    `json:"text"`
+	Username  string    `json:"user_name"`
+	UserID    string    `json:"user_id"`
+	Token     string    `json:"token"`
+	Timestamp time.Time `json:"timestamp"`
 }
